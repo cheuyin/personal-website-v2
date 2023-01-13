358a29654dc6cae025d20fbcb36b4abea1eb1ef6
cheuyin <yinstanleycheung@gmail.com>
2023-01-13 08:41:20 -0800

    Add hamburger menu icon to mobile menu

--- components/MobileNav/MobileNav.tsx
+++ components/MobileNav/MobileNav.tsx
@@ -1,13 +1,34 @@
 import styles from "./MobileNav.module.css";
+import Image from "next/image";
+import { useState } from "react";
 
 const MobileNav = () => {
+	const [opened, setOpened] = useState(true);
+
+	function toggleMenu() {
+		setOpened(prev => !prev);
+	}
+
 	return (
 		<div className={styles.container}>
-			<ul>
-				<li>
-                    Hello! I am the mobile nav bar.
-                </li>
-			</ul>
+			{opened ? (
+				<div className={styles.opened_menu}>
+					<ul>
+						<li>Hello! I am the mobile nav bar.</li>
+					</ul>
+				</div>
+			) : (
+				<div className={styles.closed_menu}>
+					<button onClick={toggleMenu}>
+						<Image
+							src="icons/bars-solid.svg"
+							alt="menu"
+							width={36}
+							height={36}
+						/>
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
