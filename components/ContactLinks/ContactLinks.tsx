e1e059b3438767b145e9be8795b1cf0cab7be14f
cheuyin <yinstanleycheung@gmail.com>
2023-01-15 09:08:15 -0800

    Add URLs to social media buttons

--- components/ContactLinks/ContactLinks.tsx
+++ components/ContactLinks/ContactLinks.tsx
@@ -4,14 +4,37 @@ import styles from "./ContactLinks.module.css";
 const ContactLinks = () => {
 	return (
 		<div className={styles.container}>
-			<Image
-				src="/icons/linkedin-icon.svg"
-				alt="linkedin"
-				width={36}
-				height={36}
-			/>
-			<Image src="/icons/github-icon.svg" alt="github" width={36} height={36} />
-			<Image src="/icons/email-icon.svg" alt="email" width={49} height={36} />
+			<a
+				href="https://www.linkedin.com/in/yinstanleycheung/"
+				target="_blank"
+				rel="noreferrer noopener"
+			>
+				<Image
+					src="/icons/linkedin-icon.svg"
+					alt="linkedin"
+					width={36}
+					height={36}
+				/>
+			</a>
+			<a
+				href="https://github.com/cheuyin"
+				target="_blank"
+				rel="noreferrer noopener"
+			>
+				<Image
+					src="/icons/github-icon.svg"
+					alt="github"
+					width={36}
+					height={36}
+				/>
+			</a>
+			<a
+				href="mailto:yinstanleycheung@gmail.com"
+				target="_blank"
+				rel="noreferrer noopener"
+			>
+				<Image src="/icons/email-icon.svg" alt="email" width={49} height={36} />
+			</a>
 		</div>
 	);
 };
