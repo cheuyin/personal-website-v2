ae37b86fe08938667f7540f39fa0bfeedb32c120
cheuyin <yinstanleycheung@gmail.com>
2023-01-14 21:09:31 -0800

    Add styling for profile picture component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,14 +1,16 @@
 import NavBar from "../NavBar/NavBar";
 import MobileNav from "../MobileNav/MobileNav";
 import styles from "./Layout.module.css";
+import React from "react";
 
-const Layout = () => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div>
 			<header>
 				<MobileNav />
 				<NavBar />
 			</header>
+			<main className={styles.main_container}>{children}</main>
 		</div>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
 import Layout from "../components/Layout/Layout";
+import Image from "next/image";
+import styles from "../styles/index.module.css";
 
 export default function Home() {
-	return <Layout />;
+	return (
+		<Layout>
+			<div className={styles.profile}>
+				<Image
+					src="/images/profile-pic-cropped.png"
+					alt="profile pic"
+					width={200}
+					height={200}
+					quality={100}
+					priority
+				/>
+				<h1>Stanley Cheung</h1>
+				<p>Full-stack developer</p>
+			</div>
+		</Layout>
+	);
 }
