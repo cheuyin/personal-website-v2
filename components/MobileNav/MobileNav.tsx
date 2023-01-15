b54dc91081270384186c9e00166578df5476a0d5
cheuyin <yinstanleycheung@gmail.com>
2023-01-14 20:40:43 -0800

    Complete styling for nav bar

--- components/MobileNav/MobileNav.tsx
+++ components/MobileNav/MobileNav.tsx
@@ -1,28 +1,43 @@
 import styles from "./MobileNav.module.css";
 import Image from "next/image";
 import { useState } from "react";
+import ContactLinks from "../ContactLinks/ContactLinks";
 
 const MobileNav = () => {
 	const [opened, setOpened] = useState(true);
 
 	function toggleMenu() {
-		setOpened(prev => !prev);
+		setOpened((prev) => !prev);
 	}
 
 	return (
 		<div className={styles.container}>
 			{opened ? (
 				<div className={styles.opened_menu}>
-					<ul>
-						<li>Hello! I am the mobile nav bar.</li>
+					<button onClick={toggleMenu}>
+						<Image
+							src="icons/xmark.svg"
+							alt="close menu"
+							width={36}
+							height={36}
+						/>
+					</button>
+					<ul className={styles.nav_links}>
+						<li>About Me</li>
+						<li>Projects</li>
+						<li>Blog</li>
+						<li>Resume</li>
 					</ul>
+					<div className={styles.links}>
+						<ContactLinks />
+					</div>
 				</div>
 			) : (
 				<div className={styles.closed_menu}>
 					<button onClick={toggleMenu}>
 						<Image
 							src="icons/bars-solid.svg"
-							alt="menu"
+							alt="open menu"
 							width={36}
 							height={36}
 						/>
