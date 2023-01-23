5d35b64d26911a671ff76698d88d865f76cc2a24
cheuyin <yinstanleycheung@gmail.com>
2023-01-23 08:04:03 -0800

    Add favicon and footer

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,15 +2,24 @@ import NavBar from "../NavBar/NavBar";
 import MobileNav from "../MobileNav/MobileNav";
 import styles from "./Layout.module.css";
 import React from "react";
+import Head from "next/head";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+	const currentYear = new Date().getFullYear();
+
 	return (
-		<div>
+		<div className={styles.layout_container}>
+			<Head>
+				<title>Stanley Cheung</title>
+			</Head>
 			<header>
 				<MobileNav />
 				<NavBar />
 			</header>
 			<main className={styles.main_container}>{children}</main>
+			<footer className={styles.footer}>
+				© {currentYear} Stanley Cheung
+			</footer>
 		</div>
 	);
 };
